ad4e0be460ab15499b51c017fda621865df42540
Brent Pedersen <bpederse@gmail.com>
2023-06-07 09:56:37 -0600

    fixes and checks

--- trgt-mc.py
+++ trgt-mc.py
@@ -9,10 +9,14 @@ def distance(a: ty.List[int], b: ty.List[int], pow=1) -> int:
     """
     return (abs(a[0] - b[0])**pow + abs(a[1] - b[1])**pow)**(1/pow)
 
+def mc(v: cyvcf2.Variant) -> ty.List[int]:
+    m = [int(x) for x in v.format("MC")[0].split(',')]
+    return m
+
 def find_distance(mom: cyvcf2.Variant, dad: cyvcf2.Variant, kid: cyvcf2.Variant, pow: int) -> ty.Tuple[int, ty.List[int]]:
-    mom_mc = mom.format("MC")[0]
-    dad_mc = dad.format("MC")[0]
-    kid_mc = dad.format("MC")[0]
+    mom_mc = mc(mom)
+    dad_mc = mc(dad)
+    kid_mc = mc(kid)
     parent_mcs = [[mom_mc[0], dad_mc[0]], [mom_mc[0], dad_mc[1]], [mom_mc[1], dad_mc[0]], [mom_mc[1], dad_mc[1]]]
     dists = [(distance(parent_mc, kid_mc, pow=pow), parent_mc) for parent_mc in parent_mcs]
     return min(dists, key=lambda x: x[0])
@@ -21,7 +25,7 @@ def vmc_fmt(variant: cyvcf2.Variant) -> str:
     """
     Return the MC format field as a string.
     """
-    return f'{",".join(str(x) for x in variant.format("MC")[0])}'
+    return f'{",".join(str(x) for x in mc(variant))}'
 
 def mc_fmt(mc: ty.List[int]) -> str:
     """
@@ -42,11 +46,13 @@ def main(mom_vcf: pathlib.Path, dad_vcf: pathlib.Path, kid_vcfs: ty.List[pathlib
     print(f"#variant\tkid_id\tmom_mc\tdad_mc\tkid_mc\tmc_dist{pow}\tparent_ht")
     for vs in zip(*vcfs):
         mom, dad = vs[:2]
-        assert mom.POS == dad.POS and mom.CHROM == dad.CHROM and mom.REF == dad.REF and mom.ALT == dad.ALT
+        assert mom.POS == dad.POS and mom.CHROM == dad.CHROM \
+                and mom.REF == dad.REF
         for i, kid in enumerate(vs[2:]):
-            assert mom.POS == kid.POS and mom.CHROM == kid.CHROM and mom.REF == kid.REF and mom.ALT == kid.ALT
+            assert mom.POS == kid.POS and mom.CHROM == kid.CHROM \
+                    and mom.REF == kid.REF
             d, parental_ht = find_distance(mom, dad, kid, pow)
-            print(f'{mom.CHROM}:{mom.POS}:{mom.REF}:{mom.ALT[0]}\t{kid_ids[i]}\t{vmc_fmt(mom)}\t{vmc_fmt(dad)}\t{vmc_fmt(kid)}\t{d}\t{mc_fmt(parental_ht)}')
+            print(f'{mom.CHROM}:{mom.POS}:{mom.REF}:{"".join(mom.ALT) or "."}\t{kid_ids[i]}\t{vmc_fmt(mom)}\t{vmc_fmt(dad)}\t{vmc_fmt(kid)}\t{d}\t{mc_fmt(parental_ht)}')
 
 
 
