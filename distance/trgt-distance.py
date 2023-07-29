940cb8ebdc49912715e846b501066754f252f7c2
Harriet Dashnow <h.dashnow@gmail.com>
2023-07-29 09:16:04 -0600

    change pow to power to avoid error caused by attempting to overwrite pow function

--- distance/trgt-distance.py
+++ distance/trgt-distance.py
@@ -7,12 +7,12 @@ import plotly.express as px
 from itertools import cycle
 
 
-def distance(a: ty.List[int], b: ty.List[int], pow=1) -> int:
+def distance(a: ty.List[int], b: ty.List[int], power=1) -> int:
     """
     Calculate the distance between two alleles.
-    pow of 1 is manhattan distance, pow of 2 is euclidean distance.
+    power of 1 is manhattan distance, power of 2 is euclidean distance.
     """
-    return sum(abs(a[i] - b[i])**pow for i in range(len(a)))**(1/pow)
+    return sum(abs(a[i] - b[i])**power for i in range(len(a)))**(1/power)
 
 def get_tag(v: cyvcf2.Variant, tag: str) -> ty.List[int]:
     if tag == "GT":
@@ -62,7 +62,7 @@ def generate_combinations(mom: ty.List[ty.List[int]], dad: ty.List[ty.List[int]]
 
 
 def find_distance(mom: cyvcf2.Variant, dad: cyvcf2.Variant, kid: cyvcf2.Variant,
-                  pow: int, tags: ty.List[str]) -> ty.Tuple[int, ty.List[int]]: #, ty.List[int]]:
+                  power: int, tags: ty.List[str]) -> ty.Tuple[int, ty.List[int]]: #, ty.List[int]]:
     mom_mc = []
     dad_mc = []
     kid_mc = []
@@ -103,14 +103,14 @@ def mc_fmt(mc: ty.List[int]) -> str:
     return f'{",".join(str(x) for x in mc)}'
 
 def main(mom_vcf: pathlib.Path, dad_vcf: pathlib.Path, kid_vcfs:
-         ty.List[pathlib.Path], *, pow: int = 1, output_prefix: str = "trgt-mc-",
+         ty.List[pathlib.Path], *, power: int = 1, output_prefix: str = "trgt-mc-",
          dist_tags: ty.List[str] = ["MC", "AL", "AP"], extra_tags: ty.List[str] = ["SD"],
          exclude_chroms: ty.List[str] = ['chrX', 'chrY']):
     """
     :param mom_vcf: Path to the maternal TRGT VCF file.
     :param dad_vcf: Path to the paternal TRGT VCF file.
     :param kid_vcfs: List of paths to one or more child TRGT VCF files.
-    :param pow: Power to use for distance calculation. 1 is manhattan distance, 2 is euclidean distance.
+    :param power: Power to use for distance calculation. 1 is manhattan distance, 2 is euclidean distance.
     :param output_prefix: prefix for output files.
     :param dist_tags: VCF format field used for lengths.
     :param exclude_chroms: chromosomes to exclude.
@@ -138,7 +138,7 @@ def main(mom_vcf: pathlib.Path, dad_vcf: pathlib.Path, kid_vcfs:
                 assert (mom.POS == kid.POS) and mom.CHROM == kid.CHROM \
                     and mom.REF == kid.REF, (mom.POS, kid.POS, mom.REF, kid.REF)
             try:
-                d, parental_ht = find_distance(mom, dad, kid, pow, dist_tags)
+                d, parental_ht = find_distance(mom, dad, kid, power, dist_tags)
             except ValueError:
                 d = -1
                 parental_ht = [-1, -1]
@@ -154,7 +154,7 @@ def main(mom_vcf: pathlib.Path, dad_vcf: pathlib.Path, kid_vcfs:
             print(line, file=fh)
 
     fig = px.histogram(x=dists)
-    m = ["manhattan", "euclidean"][pow - 1]
+    m = ["manhattan", "euclidean"][power - 1]
     fig.update_layout(xaxis_range=[-1, 25], xaxis_title=f"{m}-distance between child and parent alleles ({','.join(dist_tags)})")
     fig.write_html(output_prefix + 'dists.html')
     print(f'wrote {output_prefix}dists.html and {output_prefix}dists.txt',
