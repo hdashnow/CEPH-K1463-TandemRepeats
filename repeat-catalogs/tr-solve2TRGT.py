3394155f590e745447835edd214440a2d13eebbe
Harriet Dashnow <h.dashnow@gmail.com>
2023-09-27 18:10:21 -0600

    remove adjacent repeating motifs, set max output size and supress seq reporting by default

--- repeat-catalogs/tr-solve2TRGT.py
+++ repeat-catalogs/tr-solve2TRGT.py
@@ -6,14 +6,15 @@ import pathlib
 import subprocess
 import re
 import pysam
+from itertools import groupby
 
-def extractvcf(infile: pathlib.Path, minsize: int = 8):
+def extractvcf(infile: pathlib.Path, minins: int = 8):
     cyvcf2_vcf = cyvcf2.VCF(infile)
     for locus in cyvcf2_vcf:
-        if locus.INFO['SVTYPE'] == 'INS' and locus.INFO['SVLEN'] >= minsize:
+        if locus.INFO['SVTYPE'] == 'INS' and locus.INFO['SVLEN'] >= minins:
             yield (locus.CHROM, locus.POS, locus.POS), locus.ALT
 
-def extractfasta(bed: pathlib.Path, fasta: pathlib.Path):
+def extractfasta(bed: pathlib.Path, fasta: pathlib.Path, maxout: int = 10000):
     """
     Extract sequences from a FASTA file using a BED file.
     :param bed: Path to the BED file.
@@ -26,6 +27,8 @@ def extractfasta(bed: pathlib.Path, fasta: pathlib.Path):
     with open(bed) as f:
         for line in f:
             locus = line.strip().split()
+            if int(locus[2]) - int(locus[1]) > maxout:
+                continue
             sequence = ref.fetch(locus[0], int(locus[1]), int(locus[2]))
             yield (locus[0], locus[1], locus[2]), [sequence]
 
@@ -62,6 +65,8 @@ def runtrsolve(sequence: str, trsolve: pathlib.Path):
     maxpos = None
     motifs = []
     for motif, start, end in re_motifs:
+        assert 'N' not in motif, f'N found in motif: {motif}'
+
         motifs.append(motif)
         if minpos is None:
             minpos = int(start)
@@ -74,23 +79,36 @@ def runtrsolve(sequence: str, trsolve: pathlib.Path):
 
     return motifs, [minpos, maxpos]
 
+def rmdup(seq):
+    """remove adjacent duplicates from a list
+
+    >>> rmdup([1, 1, 2, 3, 3, 3, 4, 5, 5])
+    [1, 2, 3, 4, 5]
+    >>> rmdup(['a', 'a', 'b', 'c', 'c', 'c', 'a', 'd', 'e', 'e'])
+    ['a', 'b', 'c', 'a', 'd', 'e']
+    """
+    return [x[0] for x in groupby(seq)]
+
 def main(infile: pathlib.Path, outfile: str = 'stdout', *, 
-         fasta: pathlib.Path = None, minsize: int = 8,
-         trtools: pathlib.Path = pathlib.Path('tr-solve')):
+         fasta: pathlib.Path = None, minins: int = 8, maxout: int = 10000,
+         trtools: pathlib.Path = pathlib.Path('tr-solve'),
+         seqout: bool = False):
     """
     Convert a VCF or BED file to TRGT repeat definitions.
     :param infile: Path to the input VCF or BED file.
     :param outfile: Path to the output TRGT repeat definitions file.
     :param fasta: Path to the reference FASTA file (required for BED inputs).
-    :param minsize: Minimum insertion size to use from VCF.
+    :param minins: Minimum insertion size to use from VCF.
+    :param maxout: Maximum region size to output (smaller ones skipped).
     :param trtools: Path to the tr-solve binary.
+    :param seqout: Output all input sequences. When false, sequences with no motif found are skipped.
     """
     if infile.suffix == '.vcf' or infile.suffixes[-2:] == ['.vcf', '.gz']:
-        sequences = extractvcf(infile, minsize=minsize)
+        sequences = extractvcf(infile, minins=minins)
     elif infile.suffix == '.bed':
         if fasta is None:
             raise ValueError('BED input requires FASTA file')
-        sequences = extractfasta(infile, fasta)
+        sequences = extractfasta(infile, fasta, maxout=maxout)
     else:
         raise ValueError(f'Unknown input file type: {infile.suffix}')
     
@@ -104,7 +122,8 @@ def main(infile: pathlib.Path, outfile: str = 'stdout', *,
             motifs, bounds = runtrsolve(alt, trsolve=trtools)
             
             if len(motifs) == 0:
-                f.write(f'{alt}\tNone\n')
+                if seqout:
+                    f.write(f'{alt}\tNone\n')
                 continue
             start = int(start_orig)
             end = int(end_orig)
@@ -120,11 +139,15 @@ def main(infile: pathlib.Path, outfile: str = 'stdout', *,
             if end < int(start_orig):
                 end = int(start_orig)
 
-            unique_motifs = dict.fromkeys(motifs) # can use set(motifs) if order doesn't matter
+            unique_motifs = dict.fromkeys(motifs) # can use set(motifs) if order doesn't matter. Assume it does for now
             motifs_str = ','.join(unique_motifs)
-            struc = ''.join([f'({motif})n' for motif in motifs])
-            # XXX Delete the input sequence printing once tested - remove {alt}\t below and f.write(f'{alt}\tNone\n') above
-            f.write(f'{alt}\t{chrom}\t{start}\t{end}\tID={chrom}_{start}_{end};MOTIFS={motifs_str};STRUC={struc}\n')
+            struc = ''.join([f'({motif})n' for motif in rmdup(motifs)])
+            if seqout:
+                outstring = f'{alt}\t'
+            else:
+                outstring = ''
+            outstring += f'{chrom}\t{start}\t{end}\tID={chrom}_{start}_{end};MOTIFS={motifs_str};STRUC={struc}\n'
+            f.write(outstring)
 
     f.flush()
 
